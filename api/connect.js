0e5152f95366a12ecbcb4b6a650ac1a4f3edf568
Parshuram Bagade <parshurambagade855@gmail.com>
2023-06-14 22:26:32 +0530

    changes made

--- api/connect.js
+++ api/connect.js
@@ -1,8 +1,13 @@
 import mysql from "mysql";
+import dotenv from 'dotenv';
 
+dotenv.config(); // Load environment variables from .env file
 export const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "social",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
+
+
+
